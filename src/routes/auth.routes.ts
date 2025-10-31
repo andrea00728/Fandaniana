685146a05adec878691178b15e25a72a26075fe0
andrea00728 <andrea112samuel@gmail.com>
2025-10-31 11:45:42 +0300

    configuration timout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -33,6 +33,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+   connectionTimeout: 30000,
 });
 
 // OTP in-memory store
