807931a78eb333b2a844b55c44ab4d851ea0eb45
andrea00728 <andrea112samuel@gmail.com>
2025-10-31 10:42:39 +0300

    modification docker

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -7,10 +7,10 @@ import { ActivityType } from "../entities/ActivityType";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "dpg-d425fkpr0fns738v2280-a",
   port: Number(process.env.DB_PORT || 5432),
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "aandrea",
+  username: process.env.DB_USER || "depensepersonnel_user",
+  password: process.env.DB_PASSWORD || "fiuHfhEQUZwhHi6QsexhT8FH5gOenQaz",
   database: process.env.DB_NAME || "depensepersonnel",
   // entities: ["dist/entities/*.js"],
     entities: [Wallet,Transaction,ActivityType],
